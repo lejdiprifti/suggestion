4772032cef2ae28b3743e6eab830b4b8811294e9
lejdiprifti <priftilejdi@gmail.com>
2019-06-12 21:46:07 +0200

    added validators

--- server-app/src/main/java/com/ikubinfo/project/resource/RegisterResource.java
+++ server-app/src/main/java/com/ikubinfo/project/resource/RegisterResource.java
@@ -2,9 +2,12 @@ package com.ikubinfo.project.resource;
 
 import java.net.URISyntaxException;
 
+import javax.persistence.NoResultException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,15 +15,17 @@ import javax.ws.rs.core.Response;
 import com.ikubinfo.project.model.RegisterRequest;
 import com.ikubinfo.project.model.UserModel;
 import com.ikubinfo.project.service.RegisterService;
+import com.ikubinfo.project.service.UserService;
 
 @Path("/register")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class RegisterResource {
 	private RegisterService registerService;
-
+	private UserService userService;
 	public RegisterResource() {
 		this.registerService = new RegisterService();
+		this.userService= new UserService();
 	}
 
 	@POST
@@ -29,5 +34,15 @@ public class RegisterResource {
 			return Response.ok(registerService.register(registerRequest)).build();
 		
 
+	}
+	
+	@GET
+	@Path("/{username}")
+	public Response getUserByUsername(@PathParam("username") String username) {
+		try {
+		return Response.ok(userService.getUserByUsername(username)).build();
+		}catch (NoResultException e) {
+			return Response.noContent().build();
+		}
 	}
 }
